55f4793176e2976a629e5c01f2e504afb2bf71bd
F35 <f35idk@gmail.com>
2020-09-12 17:42:30 +0200

    implement sprite horizontal flipping

--- emulator/src/ppu/draw.rs
+++ emulator/src/ppu/draw.rs
@@ -79,9 +79,18 @@ fn draw_tile_row_background_and_sprites(
 
                         // if current dot is within x-coords of sprite
                         if tile_offset < 8 {
+                            // calculate amount to shift tile bitplanes by
+                            // to get the current pixel (depends on whether
+                            // the sprite is flipped horizontally)
+                            let shift_amt = if data.attributes & 0b01000000 != 0 {
+                                tile_offset
+                            } else {
+                                7 - tile_offset
+                            };
+
                             let color_index = {
-                                let lo = (data.tile_bitplane_low >> (7 - tile_offset)) & 1;
-                                let hi = ((data.tile_bitplane_high >> (7 - tile_offset)) << 1) & 2;
+                                let lo = (data.tile_bitplane_low >> shift_amt) & 1;
+                                let hi = ((data.tile_bitplane_high >> shift_amt) << 1) & 2;
                                 lo | hi
                             };
 
@@ -180,11 +189,19 @@ fn draw_tile_row_backdrop_color_and_sprites(ppu: &mut Ppu, pixels_to_draw: u8, d
                 .iter()
                 .take_while(|data| !data.is_end_of_array())
                 .find_map(|data| {
-                    let tile_offset = (ppu.current_scanline_dot + i as u16) - data.x as u16;
+                    let tile_offset =
+                        (ppu.current_scanline_dot + i as u16).wrapping_sub(data.x as u16);
+
                     if tile_offset < 8 {
+                        let shift_amt = if data.attributes & 0b01000000 != 0 {
+                            tile_offset
+                        } else {
+                            7 - tile_offset
+                        };
+
                         let color_index = {
-                            let lo = (data.tile_bitplane_low >> (7 - tile_offset)) & 1;
-                            let hi = ((data.tile_bitplane_high >> (7 - tile_offset)) << 1) & 2;
+                            let lo = (data.tile_bitplane_low >> shift_amt) & 1;
+                            let hi = ((data.tile_bitplane_high >> shift_amt) << 1) & 2;
                             lo | hi
                         };
 
