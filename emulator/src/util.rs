aa7ebf53231734b471709fd3ee488cf20f25f4a9
F35 <f35idk@gmail.com>
2020-11-29 21:14:03 +0100

    add comment clarifying cast to higher-aligned ptr
    and tell clippy to ignore the cast

--- emulator/src/util.rs
+++ emulator/src/util.rs
@@ -1,7 +1,13 @@
 use crate::PixelRenderer;
 
+#[allow(clippy::cast_ptr_alignment)]
 pub fn pixels_to_u32<'a>(pixel_renderer: &'a mut PixelRenderer) -> &'a mut [u32; 256 * 240] {
-    unsafe { std::mem::transmute(pixel_renderer.get_pixels().as_mut_ptr() as *mut u32) }
+    debug_assert_eq!((pixel_renderer.get_pixels().as_mut_ptr() as usize) % 64, 0);
+    // SAFETY: the 'get_pixels()' byte slice is always aligned to at least 64
+    // bytes, making the cast safe. this is ensured by the vulkan implementation
+    // itself ('PixelRenderer' uses vulkan). see table 52, 'required limits' in:
+    // https://www.khronos.org/registry/vulkan/specs/1.2-extensions/html/vkspec.html#memory-device-hostaccess
+    unsafe { &mut *(pixel_renderer.get_pixels().as_mut_ptr() as *mut _) }
 }
 
 macro_rules! error_exit {
